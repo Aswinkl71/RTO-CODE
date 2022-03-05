ffc921c221b59ace8091dd89b4f036d13ff42d00
Aswinkl71 <93024193+Aswinkl71@users.noreply.github.com>
2022-03-05 21:47:21 +0530

    Add files via upload

---
+++ php[1].php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" type="text/css" href="index.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   <title>Document</title>
+</head>
+
+<body>
+<?php
+   $num1= $_POST["num1"];
+$KL = array("01"=>"THIRUVANANTHAPURAM","02"=>"KOLLAM","03"=>"PATHANAMTHITTA","04"=>"ALAPPUZHA","05"=>"KOTTAYAM","KL06"=>"IDUKKI","07"=>"ERNAKULAM","08"=>"THRISSUR","09"=>"PALAKKAD","10"=>"MALAPPURAM","11"=>"KOZHIKODE","12"=>"WAYANAD","13"=>"KANNUR","14"=>"KASARAGOD","15"=>"NATIONAL SECTOR","16"=>"ATTINGAL","17"=>"MUVATTUPUZHA","18"=>"VADAKARA","19"=>"PARASSALA","20"=>"NEYYATTINKARA","21"=>"NEDUMANGAD","22"=>"KAZHAKUTTOM","23"=>"KARUNAGAPPALLY","24"=>"KOTTARAKKARA","25"=>"PUNALUR","26"=>"ADOOR","27"=>"THIRUVALLA","28"=>"MALLAPPALLY","29"=>"KAYAMKULAM","30"=>"CHENGANNUR","KL31"=>"MAVELIKKARA","32"=>"CHERTHALA","33"=>"CHANGANACHERRY","34"=>"KANJIRAPPALLY","35"=>"PALA","36"=>"VAIKKOM","37"=>"VANDIPERIYAR","38"=>"THODUPUZHA","39"=>"TRIPUNITHURA","40"=>"PERUMBAVOOR","41"=>"ALUVA","42"=>"NORTH PARAVOOR","43"=>"MATTANCHERRY","44"=>"KOTHAMANGALAM","45"=>"IRANJALAKKUDA","46"=>"GURUVAYOOR","47"=>"KODUNGALLUR","48"=>"VADAKKANCHERRY","49"=>"ALATHUR","50"=>"MANNARKKAD","51"=>"OTTAPPALAM","52"=>"PATTAMBI","53"=>"PERINTHALMANNA","54"=>"PONNANI","55"=>"TIRUR","56"=>"KOYILANDY","57"=>"KODUVALLY","58"=>"THALASSERY","59"=>"THALIPARAMBA","60"=>"KANHANGAD","61"=>"KUNNATHUR","62"=>"RANNI","63"=>"ANGAMALI","64"=>"CHALAKKUDY","65"=>"THIRURANGADI","66"=>"KUTTANADU","67"=>"UZHAVOOR","68"=>"DEVIKULAM","69"=>"UDUMBANCHOLA","70"=>"CHITTUR","71"=>"NILAMBUR","72"=>"MANANTHAVADY","73"=>"SULTHANBATHERY","74"=>"KATTAKKADA","75"=>"THRIPRAYAR","76"=>"NANMANDA","77"=>"PERAMBRA","78"=>"IRITTY","79"=>"VELLARIKUNDU","80"=>"PATHANAPURAM","81"=>"VARKALA","82"=>"CHADAYAMANGALAM","83"=>"KONNI","84"=>"KONDOTTY","85"=>"RAMANATTUKARA (FEROKE)","86"=>"PAYYANNUR");
+
+echo "<font color='blue'>Kerala RTO ==></font>";
+echo  $KL [$num1];
+   ?>
+   <br>
+   <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://instagram.com/aswinkl71?utm_medium=copy_link" class="fa fa-instagram"></a>
+   &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.youtube.com/channel/UCKunuCovFZK9ZG4KbfAh_fg" class="fa fa-youtube"></a>
+
+
+</body>
+</html>
+
